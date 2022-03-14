ee2c4bd8ec4b1d1d7de3a6a450b1734b5af41f85
X-AnTa <mrspbusdriver@gmail.com>
2022-03-14 19:22:48 +0600

    Spring Boot and DB

---
+++ src/main/java/com/anta/news/NewsForEmployerApplication.java
@@ -0,0 +1,13 @@
+package com.anta.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsForEmployerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsForEmployerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anta/news/NewsForEmployerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anta.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsForEmployerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
